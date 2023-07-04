9e724c5eeede182493df2806d1b85218ba349044
J4sTFly <alex.hrynts@gmail.com>
2023-07-04 19:16:50 -0400

    Email validation fix

--- app/models/concerns/email_validatable.rb
+++ app/models/concerns/email_validatable.rb
@@ -1,16 +1,25 @@
 require 'mail'
-
+# Module for User email validation
 module EmailValidatable
   extend ActiveSupport::Concern
 
+  # Validates User email
+  # Usage: validates :email, email:true
   class EmailValidator < ActiveModel::EachValidator
+    def validation_error(record, attribute)
+      record.errors.add(attribute, (options[:message] || 'is not an email'))
+    end
+
     def validate_each(record, attribute, value)
       begin
         email = Mail::Address.new(value)
-        email.address
       rescue Mail::Field::ParseError
-        record.errors[attribute] << (options[:message] || "is not an email")
+        validation_error(record, attribute)
       end
+      value = email&.address
+      return if value =~ /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+
+      validation_error(record, attribute)
     end
   end
 end
